bd7125c0d25fd2a0203c6696f0f314d65ad59ac0
Aryan chaurasia <aryachaurasi20@gmail.com>
2025-02-21 15:32:00 +0530

    Aryan chaurasia: add- Made REST Call to show Hello Mark from BridgeLabz

--- RespondHelloMessage/src/main/java/org/example/demo/controller/HelloRestController.java
+++ RespondHelloMessage/src/main/java/org/example/demo/controller/HelloRestController.java
@@ -1,6 +1,8 @@
 package org.example.demo.controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +12,9 @@ public class HelloRestController {
     @GetMapping
     public String sayHello() {
         return "Hello from BridgeLabz";
+    }
+    @GetMapping("/query")
+    public String sayHelloWithName(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
     }
 }
